98b1af82c87734270ec61a115d67c5bf730616ae
jmlinares2019 <jlinaresmar@gmail.com>
2024-02-29 18:16:32 +0100

    Changing views filepaths

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
-import url from 'node:url'
 import axios from 'axios';
 import bodyParser from 'body-parser';
 
@@ -7,6 +8,9 @@ const app = express();
 const port = 3000;
 const API_URL = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json';
 const API_KEY = '4c0ee450-6bb8-4361-8622-585b4b1e598d';
+
+// create filepath
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // setting static files location
 app.use(express.static("public"));
@@ -16,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Search page (home)
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render(`${__dirname}/src/views/index.ejs`);
 });
 
 // Search results
@@ -28,12 +32,12 @@ app.post("/search", async (req, res) => {
         const results = response.data;
         // Checking whether we receive full result or array of suggestions (for wrong or incomplete search)
         if(results[0].meta){
-            res.render("results.ejs", { 
+            res.render(`${__dirname}/src/views/results.ejs`, { 
                 entry: search,
                 results: results 
             });
         } else {
-            res.render("suggestions.ejs", { 
+            res.render(`${__dirname}/src/views/suggestions.ejs`, { 
                 search: search,
                 suggestions: results 
             });
@@ -53,12 +57,12 @@ app.get("/:word", async (req, res) => {
         const results = response.data;
             // Checking whether we receive full result or array of suggestions (for wrong or incomplete search)
             if(results[0].meta){
-                res.render("results.ejs", { 
+                res.render(`${__dirname}/src/views/results.ejs`, { 
                     entry: search,
                     results: results 
                 });
             } else {
-                res.render("suggestions.ejs", { 
+                res.render(`${__dirname}/src/views/suggestions.ejs`, { 
                     search: search,
                     suggestions: results 
                 });
